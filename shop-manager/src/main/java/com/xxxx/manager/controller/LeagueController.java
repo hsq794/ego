6863f8a73f6d3829ae7dccd7640229a401e4eb3c
cl <3328177285@qq.com>
2020-06-10 10:13:31 +0800

    积分排行榜所需实体类

---
+++ shop-manager/src/main/java/com/xxxx/manager/controller/LeagueController.java
@@ -0,0 +1,19 @@
+package com.xxxx.manager.controller;
+
+import com.xxxx.common.result.BaseResult;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("league")
+public class LeagueController {
+
+    /**
+     * 添加用户数据
+     * @return
+     */
+    public BaseResult addUser(){
+        return null;
+    }
+
+}
