55eaa12329027e9116bdb5285aded4d56d1ea59f
cl <3328177285@qq.com>
2020-06-10 14:59:38 +0800

    积分排行榜中添加假的用户数据

--- shop-manager/src/main/java/com/xxxx/manager/service/UserService.java
+++ shop-manager/src/main/java/com/xxxx/manager/service/UserService.java
@@ -4,8 +4,12 @@ import com.xxxx.common.result.BaseResult;
 import com.xxxx.manager.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,11 +18,12 @@ public class UserService {
     @Autowired
     private RedisTemplate<String,Object> redisTemplate;
 
+
+
     /**
      * 添加用户
      * @return
      */
-
     public BaseResult addUser() {
         User[] users = new User[]{
                 new User(1,"张一"),
@@ -34,11 +39,48 @@ public class UserService {
         };
         //清空数据库
         redisTemplate.getConnectionFactory().getConnection().flushDb();
-        List<Integer> userList = Stream.of(users).map(user -> {
+
+        List<Integer> userIdList = Stream.of(users).map(user -> {
+            //将数据存到redis
             redisTemplate.opsForHash().put("user", user.getUserId(), user);
             return user.getUserId();
         }).collect(Collectors.toList());
 
-        return null;
+        userIdList.forEach(userId->{
+            //生成随机积分
+            int score = ThreadLocalRandom.current().nextInt(100);
+            addcore(userId, score);
+
+        });
+        return userIdList.size()>0?BaseResult.success():BaseResult.error();
     }
+
+    /**
+     * 添加用户积分
+     * @param userId
+     * @param score
+     * @return
+     */
+    public String addcore(Integer userId,Integer score){
+        if(StringUtils.isEmpty(userId)){
+            return "error";
+        }
+        if(StringUtils.isEmpty(score)){
+            return "error";
+        }
+        //获取积分key
+        String key = getScoreKey();
+        //添加用户积分到redis并排序
+        redisTemplate.opsForZSet().incrementScore(key, userId, score);
+        return "success";
+    }
+
+    /**
+     * 积分key
+     */
+    public String getScoreKey(){
+        String yyyyMM = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMM"));
+        return String.format("rank:%s",yyyyMM);
+    }
+
 }
